976770ebd19a348a6d4e6ea9f56210ab97080e19
Este <anthony.garces1995@gmail.com>
2019-11-10 18:55:55 -0600

    Added some styling and reformatted my HTML

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var city = $("#Search-bar");
+var city = $("#Search-Bar");
 APIKey = "ed1ac3232ca7f1df342053eaf80be5cb";
 var queryWeaURL = `https://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=` + APIKey;
 var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=` + APIKey;
@@ -18,7 +18,7 @@ $("#Submitbtn").click(
             method: "GET"
         }).then(function(response) {
             // $("#City").text(city.val());
-            // console.log(city.val())
+            console.log(city.val())
             $("#Temperature").text(response.main.temp);
             $("#Humidity").text(response.main.humidity);
             $("#WindSpeed").text(response.wind.speed);
