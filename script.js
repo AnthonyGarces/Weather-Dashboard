9520355ab78ac1de6a3bed651137bba104b2a17b
Este <anthony.garces1995@gmail.com>
2019-11-11 10:46:34 -0600

    Added separate ajax request for UV index

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var city = $("#Search-Bar");
+var city = $("#Search-Bar").val();
 APIKey = "ed1ac3232ca7f1df342053eaf80be5cb";
+var lat = "";
+var lon = "";
 var queryWeaURL = `https://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=` + APIKey;
-var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=` + APIKey;
+// var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city.val()}&units=imperial&appid=` + APIKey;
+var queryUVURL = `http://api.openweathermap.org/data/2.5/uvi?lat=30.27&lon=-97.74&APPID=` + APIKey;
+
 
 //city var not working
 function dispTime() {
@@ -13,17 +17,23 @@ $("#Submitbtn").click(
     function() {
         event.preventDefault();
         dispTime();
+        $("#City").text(city);
         $.ajax({
             url: queryWeaURL,
             method: "GET"
         }).then(function(response) {
-            // $("#City").text(city.val());
-            console.log(city.val())
             $("#Temperature").text(response.main.temp);
             $("#Humidity").text(response.main.humidity);
             $("#WindSpeed").text(response.wind.speed);
-            //Where to find UV Index?
-            // $("#UVIndex").text(response.main.temp);
+            lat = response.coord.lat;
+            lon = response.coord.lon
+            $.ajax({
+                url: queryUVURL,
+                method: "GET"
+            }).then(function(yonge) {
+                $("#UVIndex").text(yonge.value);
+            })
+            
             
         });
         
