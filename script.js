94a09833079c66677a12505f0b4bf43ca7fa39ba
Este <anthony.garces1995@gmail.com>
2019-11-12 18:33:25 -0600

    fixed bug where search bar was returning a bad ajax request, now functioning

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-var city = $("#Search-Bar");
+var city = "";
 APIKey = "ed1ac3232ca7f1df342053eaf80be5cb";
 var lat = "";
 var lon = "";
-var queryWeaURL = "https://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=" + APIKey;
-var queryForURL = "https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=" + APIKey;
-var queryUVURL = "http://api.openweathermap.org/data/2.5/uvi?lat=29.76&lon=-95.37&APPID=" + APIKey;
 var cityList = [];
 
 
@@ -17,21 +14,25 @@ function dispTime() {
 $("#Submitbtn").click(
     function() {
         event.preventDefault();
+        city = $("#Search-Bar").val();
+        var queryWeaURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=` + APIKey;
+        var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=` + APIKey;
         dispTime();
-        cityList.push(city.val().trim());
+        cityList.push(city);
         renderButton();
-        console.log(city.val())
-        $("#City").text(city.val());
+        $("#City").text(city);
         $.ajax({
             url: queryWeaURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
+            console.log("response", response)
             $("#Temperature").text(response.main.temp);
             $("#Humidity").text(response.main.humidity);
             $("#WindSpeed").text(response.wind.speed);
-            // lat = response.coord.lat;
-            // lon = response.coord.lon
+            lat = response.coord.lat;
+            lon = response.coord.lon;           
+            var queryUVURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=` + APIKey;
+
             $.ajax({
                 url: queryUVURL,
                 method: "GET"
