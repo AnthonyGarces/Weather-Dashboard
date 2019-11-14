600b19b29e7999a5f864ae0e6d6a1487b2ee43cb
Este <anthony.garces1995@gmail.com>
2019-11-13 20:48:47 -0600

    Added weather icon images to the daily and 5 day forecasts

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var city = "";
 APIKey = "ed1ac3232ca7f1df342053eaf80be5cb";
 var lat = "";
 var lon = "";
+var iconCode = "";
 var cityList = [];
 
 
-//city var not working
 function dispTime() {
     $("#Date").text(JSON.stringify(moment().format('dddd, MMMM Do')));
     $("#5-Date1").text(JSON.stringify(moment().add(1, 'days').format('MMMM Do')));
@@ -57,6 +57,9 @@ function getWeather() {
         url: queryWeaURL,
         method: "GET"
     }).then(function(response) {
+        iconCode = response.weather[0].icon;
+        var iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
+        $("#todayIcon").attr('src', iconURL);
         $("#Temperature").text(response.main.temp);
         $("#Humidity").text(response.main.humidity);
         $("#WindSpeed").text(response.wind.speed);
@@ -79,6 +82,22 @@ function get5Day() {
         url: queryForURL,
         method: "GET"
     }).then(function(fivedayresponse) {
+        var fiveIconList = 
+        [fivedayresponse.list[1].weather[0].icon,
+        fivedayresponse.list[2].weather[0].icon,
+        fivedayresponse.list[3].weather[0].icon,
+        fivedayresponse.list[4].weather[0].icon,
+        fivedayresponse.list[5].weather[0].icon];
+        var IconUrl1 = "http://openweathermap.org/img/w/" + fiveIconList[0] + ".png";
+        var IconUrl2 = "http://openweathermap.org/img/w/" + fiveIconList[1] + ".png";
+        var IconUrl3 = "http://openweathermap.org/img/w/" + fiveIconList[2] + ".png";
+        var IconUrl4 = "http://openweathermap.org/img/w/" + fiveIconList[3] + ".png";
+        var IconUrl5 = "http://openweathermap.org/img/w/" + fiveIconList[4] + ".png";
+        $("#5-Icon1").attr("src", IconUrl1);
+        $("#5-Icon2").attr("src", IconUrl2);
+        $("#5-Icon3").attr("src", IconUrl3);
+        $("#5-Icon4").attr("src", IconUrl4);
+        $("#5-Icon5").attr("src", IconUrl5);
         $("#5-Temp1").text(fivedayresponse.list[1].main.temp);
         $("#5-Hum1").text(fivedayresponse.list[1].main.humidity);
         $("#5-Temp2").text(fivedayresponse.list[2].main.temp);
