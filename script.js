a58adef4ee9967ace366efc36363ed88c63d48bf
Este <anthony.garces1995@gmail.com>
2019-11-13 20:26:12 -0600

    App now displays correct dates in the daily and 5 day forecasts

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ var cityList = [];
 
 //city var not working
 function dispTime() {
-    $("#Date").text(JSON.stringify(moment().format('dddd, MMMM Do,')))
+    $("#Date").text(JSON.stringify(moment().format('dddd, MMMM Do')));
+    $("#5-Date1").text(JSON.stringify(moment().add(1, 'days').format('MMMM Do')));
+    $("#5-Date2").text(JSON.stringify(moment().add(2, 'days').format('MMMM Do')));
+    $("#5-Date3").text(JSON.stringify(moment().add(3, 'days').format('MMMM Do')));
+    $("#5-Date4").text(JSON.stringify(moment().add(4, 'days').format('MMMM Do')));
+    $("#5-Date5").text(JSON.stringify(moment().add(5, 'days').format('MMMM Do')));
 }
 
 //when the button is clicked
@@ -15,46 +20,12 @@ $("#Submitbtn").click(
     function() {
         event.preventDefault();
         city = $("#Search-Bar").val();
-        var queryWeaURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=` + APIKey;
-        var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=` + APIKey;
         dispTime();
         cityList.push(city);
         renderButton();
         $("#City").text(city);
-        $.ajax({
-            url: queryWeaURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log("response", response)
-            $("#Temperature").text(response.main.temp);
-            $("#Humidity").text(response.main.humidity);
-            $("#WindSpeed").text(response.wind.speed);
-            lat = response.coord.lat;
-            lon = response.coord.lon;           
-            var queryUVURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=` + APIKey;
-
-            $.ajax({
-                url: queryUVURL,
-                method: "GET"
-            }).then(function(uvresponse) {
-                $("#UVIndex").text(uvresponse.value);
-            })  
-        });
-        $.ajax({
-            url: queryForURL,
-            method: "GET"
-        }).then(function(fivedayresponse) {
-            $("#5-Temp1").text(fivedayresponse.list[1].main.temp);
-            $("#5-Hum1").text(fivedayresponse.list[1].main.humidity);
-            $("#5-Temp2").text(fivedayresponse.list[2].main.temp);
-            $("#5-Hum2").text(fivedayresponse.list[2].main.humidity);
-            $("#5-Temp3").text(fivedayresponse.list[3].main.temp);
-            $("#5-Hum3").text(fivedayresponse.list[3].main.humidity);
-            $("#5-Temp4").text(fivedayresponse.list[4].main.temp);
-            $("#5-Hum4").text(fivedayresponse.list[4].main.humidity);
-            $("#5-Temp5").text(fivedayresponse.list[5].main.temp);
-            $("#5-Hum5").text(fivedayresponse.list[5].main.humidity);
-        });
+        getWeather();
+        get5Day();
     }
 );
 
@@ -63,8 +34,60 @@ function renderButton() {
     for (var i = 0; i < cityList.length; i++) {
         var a = $("<button>");
         a.text(cityList[i]);
-        a.addClass("btn btn-secondary mt-3");
+        a.addClass("btn btn-secondary mt-3 HBtn");
         $(".history").prepend(a);
     }
 }
 
+$(".HBtn").click(
+    function() {
+        event.preventDefault();
+        console.log(this);
+        // city = $(".HistButton").val().trim();
+        // dispTime();
+        // $("#City").text(city);
+        // getWeather();
+        // get5Day();
+    }
+);
+
+function getWeather() {
+    var queryWeaURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=` + APIKey;
+    $.ajax({
+        url: queryWeaURL,
+        method: "GET"
+    }).then(function(response) {
+        $("#Temperature").text(response.main.temp);
+        $("#Humidity").text(response.main.humidity);
+        $("#WindSpeed").text(response.wind.speed);
+        lat = response.coord.lat;
+        lon = response.coord.lon;           
+        var queryUVURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=` + APIKey;
+
+        $.ajax({
+            url: queryUVURL,
+            method: "GET"
+        }).then(function(uvresponse) {
+            $("#UVIndex").text(uvresponse.value);
+        })  
+    });
+}
+
+function get5Day() {
+    var queryForURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=` + APIKey;
+    $.ajax({
+        url: queryForURL,
+        method: "GET"
+    }).then(function(fivedayresponse) {
+        $("#5-Temp1").text(fivedayresponse.list[1].main.temp);
+        $("#5-Hum1").text(fivedayresponse.list[1].main.humidity);
+        $("#5-Temp2").text(fivedayresponse.list[2].main.temp);
+        $("#5-Hum2").text(fivedayresponse.list[2].main.humidity);
+        $("#5-Temp3").text(fivedayresponse.list[3].main.temp);
+        $("#5-Hum3").text(fivedayresponse.list[3].main.humidity);
+        $("#5-Temp4").text(fivedayresponse.list[4].main.temp);
+        $("#5-Hum4").text(fivedayresponse.list[4].main.humidity);
+        $("#5-Temp5").text(fivedayresponse.list[5].main.temp);
+        $("#5-Hum5").text(fivedayresponse.list[5].main.humidity);
+    });
+}
